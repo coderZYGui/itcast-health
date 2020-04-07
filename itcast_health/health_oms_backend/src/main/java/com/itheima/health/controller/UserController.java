66de6a9f7ed5135fc257cf3d0df2412bc05d6ef0
zygui_g <zygui@163.com>
2020-04-07 15:09:25 +0800

    health_day9_登录后显示用户名

--- itcast_health/health_oms_backend/src/main/java/com/itheima/health/controller/UserController.java
+++ itcast_health/health_oms_backend/src/main/java/com/itheima/health/controller/UserController.java
@@ -4,6 +4,9 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.itheima.health.common.MessageConst;
 import com.itheima.health.entity.Result;
 import com.itheima.health.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +46,20 @@ public class UserController {
     @RequestMapping("/loginFail")
     public Result loginFail(){
         return new Result(false, "登陆失败");
+    }
+
+    @RequestMapping("/getUsername")
+    public Result getUsername(){
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication.getPrincipal() == null){
+                return new Result(false, "获取用户名失败!");
+            }
+            User user = (User) authentication.getPrincipal();
+            return new Result(true, MessageConst.ACTION_SUCCESS, user.getUsername());
+        } catch (Exception e){
+            e.printStackTrace();
+            return new Result(false, MessageConst.ACTION_FAIL);
+        }
     }
 }
